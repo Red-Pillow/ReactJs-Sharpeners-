74e7f1136ec06de1f8cd5be5892e1d83a826917c
Red-Pillow <sahu5chetan5@gmail.com>
2025-01-29 12:03:09 +0530

    Props Object & Passing Data to Child Component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,29 @@ function App() {
   return (
     <div >
       <h1>Lets get started</h1>
-      <ExpenseItem/>
+      <ExpenseItem
+      date = {new Date(2023,7,15)}
+      title = {"insurance"}
+      price = "50"
+      location ="Bangalore" 
+      />
+      <ExpenseItem
+      date = {new Date(2023,3,25)}
+      title = "Book"
+      price = "20"
+      location ="Odisha"
+      />
+      <ExpenseItem
+      date = {new Date(2025,5,8)}
+      title = "Pen"
+      price = '44'
+      location = "Hydrabad"/>
+      
+      <ExpenseItem
+      date = {new Date(2024,5,9)}
+      title = "laptop"
+      price = "59000"
+      location = "Gurgaon"/>
     </div>
   );
 }
