584dce8c8f1f1d442d0142d5c8a6c1338ea3be3a
Red-Pillow <sahu5chetan5@gmail.com>
2025-01-29 13:38:09 +0530

    Creating Reusable Components

--- src/App.js
+++ src/App.js
@@ -1,32 +1,26 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+
+  const expenses = [
+    {id:"1",date: new Date(2023,7,15), title: "Insurance", price: 100},
+    {id:"2",date: new Date(2023,3,25), title: "Book", price: 200},
+    {id:"3",date: new Date(2024,5,8), title:"Pen", price: 10},
+    {id:"4",date: new Date(2025,7,9), title: "Laptop",price:78000}
+  ]
   return (
     <div >
       <h1>Lets get started</h1>
-      <ExpenseItem
-      date = {new Date(2023,7,15)}
-      title = {"insurance"}
-      price = "50"
-      location ="Bangalore" 
-      />
-      <ExpenseItem
-      date = {new Date(2023,3,25)}
-      title = "Book"
-      price = "20"
-      location ="Odisha"
-      />
-      <ExpenseItem
-      date = {new Date(2025,5,8)}
-      title = "Pen"
-      price = '44'
-      location = "Hydrabad"/>
-      
-      <ExpenseItem
-      date = {new Date(2024,5,9)}
-      title = "laptop"
-      price = "59000"
-      location = "Gurgaon"/>
+      {expenses.map((expense)=>{
+        return (
+          <ExpenseItem
+          date = {expense.date}
+          location = {expense.location}
+          title = {expense.title}
+          price = {expense.price}
+          />
+        )
+      })}
     </div>
   );
 }
