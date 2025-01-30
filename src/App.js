73aa56cf9592a480f7a1eed1230c43798975679f
Red-Pillow <sahu5chetan5@gmail.com>
2025-01-30 13:05:25 +0530

    Splitting the Components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
+// Write your code at relevant places in the code below
+
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
-
   const expenses = [
-    {id:"1",date: new Date(2023,7,15), location: "Bangalore",title: "Insurance", price: 100},
-    {id:"2",date: new Date(2023,3,25), location: "Delhi",title: "Book", price: 200},
-    {id:"3",date: new Date(2024,5,8), location:"Hydrabad",title:"Pen", price: 10},
-    {id:"4",date: new Date(2025,7,9), location:"Mumbai",title: "Laptop",price:78000}
-  ]
+    { id: "1", date: new Date(2023, 7, 15), title: "Insurance", price: 100 },
+    { id: "2", date: new Date(2023, 3, 25), title: "Book", price: 10 },
+    { id: "3", date: new Date(2023, 10, 11), title: "Pen", price: 1 },
+    { id: "4", date: new Date(2023, 1, 14), title: "Laptop", price: 200 },
+  ];
+
   return (
-    <div >
-      <h1>Lets get started</h1>
-      {expenses.map((expense)=>{
+    <div>
+      <h1>Let's get Started</h1>
+      {expenses.map((expense, index) => {
         return (
           <ExpenseItem
-          key={expense.id}
-          date = {expense.date}
-          location = {expense.location}
-          title = {expense.title}
-          price = {expense.price}
-          />
-        )
+            key={expense.id}
+            date={expense.date}
+            title={expense.title}
+            price={expense.price}
+          ></ExpenseItem>
+        );
       })}
     </div>
   );
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,47 +1,20 @@
-// Write your code here
+// Write your code at relevant places in the code below
 
-import './ExpenseItem.css';
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
-const months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December"]
+
 function ExpenseItem(props) {
   
-  const expenseTitle = props.title
-  const expenseAmount = props.price
-  const expenseLocation = props.location
-
-  const month = months[props.date.getMonth()]
-
-  
-  const year = props.date.getFullYear()
-  const date = props.date.getDate().toString().padStart(2, '0');
   return (
     <div className="expense-item">
-     <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{date}</div>
-     </div>
-      <div className="expense-item__location">{expenseLocation}</div>
+      <ExpenseDate calendarDate ={props.date} />
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.price}</div>
       </div>
     </div>
   );
 }
 
-
-
-export default ExpenseItem
+export default ExpenseItem;
